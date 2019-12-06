cc2260db4734ab3ecc52804e4d4b60d63f9b60b2
rebekkabb <breedis.rebekka@gmail.com>
2019-12-06 19:29:25 +0200

    Test number 4 completed, added a test to Footer to increase our "Statements coverage"

--- tests/unit/Footer.spec.js
+++ tests/unit/Footer.spec.js
@@ -1,13 +1,24 @@
-import { mount } from '@vue/test-utils'
+import {mount} from '@vue/test-utils'
 import Footer from "../../src/components/Footer.vue"
 
 describe('Footer tests', () => {
 
-    const wrapper = mount(Footer);
+    const wrapper = mount(Footer, {
+        propsData: {
+            // Add stub function to test dissapearence of whatever you finish this text
+            add: () => {
+            }
+        }
+    });
 
     it('Check when "+" button is clicked in Footer component "open" property in Footer component changes to true', () => {
         wrapper.find('span').trigger('click');
         expect(wrapper.vm.$data.open);
-    })
+    });
 
+    it('Check if after adding a todo item we clear the textbox and close it back (Rewrote the add function, because it has to to with the non-working server stuff)', () => {
+        wrapper.find('input').trigger('keyup.enter');
+        expect(wrapper.vm.$data.title).toBe("");
+        expect(!wrapper.vm.$data.open);
+    });
 });
--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils'
 import List from "../../src/components/List.vue"
+import Item from "../../src/models/Item.js"
 
 describe('List tests', () => {
 
@@ -10,6 +11,19 @@ describe('List tests', () => {
         let listEmptyText = "Add your first Todo task";
         expect(wrapper.html()).toContain(listEmptyText);
         
-    })
+    });
 
+    it('Check if an item in the list is marked as done  item is updated correctly', async () => {
+        expect(wrapper.props().list).toEqual([]);
+
+        wrapper.props().list.push(new Item("Something"));
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.props().list[0].done).toBe(false);
+
+        wrapper.find('span').trigger('click');
+        await wrapper.vm.$nextTick();
+
+        expect(wrapper.props().list[0].done).toBe(true);
+    });
 });
